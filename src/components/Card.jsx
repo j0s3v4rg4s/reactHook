9950ec988f9582afdb1cebd3f3275f9d902b5464
Jose G Vargas <ruedagato@gmail.com>
2020-12-30 20:48:34 -0500

    update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import '../styles/card.scss';
+import LikeButton from './LikeButton';
 
 function Card({ character }) {
-  return (
-    <div className="Card">
-      <img src={character.image} className="Card-img" alt="logo" />
-      <div className="Card-content">
-        <p className="Card-content__title">{character.name}</p>
-        <p className="Card-content__section">
-          Species: <span className="value">{character.species}</span>
-        </p>
-        <p className="Card-content__section">
-          Type: <span className="value">{character.type}</span>
-        </p>
-      </div>
-    </div>
-  );
+    return (
+        <div className="Card">
+            <img src={character.image} className="Card-img" alt="logo" />
+            <div className="Card-content">
+                <p className="Card-content__title">{character.name}</p>
+                <p className="Card-content__section">
+                    Species: <span className="value">{character.species}</span>
+                </p>
+                <p className="Card-content__section">
+                    Type: <span className="value">{character.type}</span>
+                </p>
+            </div>
+            <div>
+                <LikeButton />
+            </div>
+        </div>
+    );
 }
 
 export default Card;
